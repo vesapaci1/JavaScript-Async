1bc6bc38fd48ee310e1bca956971be5896041b32
vesapaci1 <vesa.paci@lowerthecurve.com>
2026-02-25 10:14:11 +0100

    Index.js Completed

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 console.log("Start");
 
+// 1. CALLBACKS
+function fetchUserCallback(callback) {
+  setTimeout(() => {
+    const success = true;
+
+    if (success) {
+      callback(null, { id: 1, name: "Vesa" });
+    } else {
+      callback("Failed to fetch user", null);
+    }
+  }, 2000);
+}
+
+function fetchOrdersCallback(userId, callback) {
+  setTimeout(() => {
+    callback(null, ["Order1", "Order2"]);
+  }, 1500);
+}
+
+function loadDataCallbacks() {
+  fetchUserCallback((err, user) => {
+    if (err) {
+      console.log("Error:", err);
+      return;
+    }
+    console.log("User:", user);
+
+    fetchOrdersCallback(user.id, (err, orders) => {
+      if (err) {
+        console.log("Error:", err);
+        return;
+      }
+      console.log("Orders:", orders);
+    });
+  });
+}
+
+// loadDataCallbacks(); // Uncomment to run callbacks version
+
+//2. PROMISES
 function fetchUserPromise() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -14,33 +54,43 @@ function fetchUserPromise() {
   });
 }
 
-fetchUserPromise()
-  .then(user => {
-    console.log("User:", user);
-  })
-  .catch(error => {
-    console.log("Error:", error);
-  });
-
-console.log("End");
-
-
 function fetchOrdersPromise(userId) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(["Order1", "Order2"]);
-        }, 1500);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(["Order1", "Order2"]);
+    }, 1500);
+  });
+}
+
+function loadDataPromises() {
+  fetchUserPromise()
+    .then((user) => {
+      console.log("User:", user);
+      return fetchOrdersPromise(user.id);
+    })
+    .then((orders) => {
+      console.log("Orders:", orders);
+    })
+    .catch((error) => {
+      console.log("Error:", error);
     });
 }
 
-fetchOrdersPromise()
-.then((user) => {
+// loadDataPromises(); // Uncomment to run Promises version
+
+// 3. ASYNC/AWAIT 
+async function loadData() {
+  try {
+    const user = await fetchUserPromise();
     console.log("User:", user);
-    return fetchOrdersPromise(user.id);
-})
-.then((orders) => {
+
+    const orders = await fetchOrdersPromise(user.id);
     console.log("Orders:", orders);
-})
-.catch((error) => {
+  } catch (error) {
     console.log("Error:", error);
-});
+  }
+}
+
+loadData();
+
+console.log("End");
